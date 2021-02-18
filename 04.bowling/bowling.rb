0bf352fa7492b84fbb780957d0de4280f966e7a9
emiko <31835314+siroemk@users.noreply.github.com>
2021-02-18 16:57:33 +0900

    フレームの値を計算するコードを追加

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,15 +1,18 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 score = ARGV[0]
 scores = score.split(',')
+
 shots = []
 frames = []
 scores.each do |s|
-  if s == 'X' # strike
-    shots << 10
-  else
-    shots << s.to_i
-  end
+  shots <<
+    if s == 'X'
+      10
+    else
+      s.to_i
+    end
 
   if shots.count == 1 && shots.first == 10
     frames << [10]
@@ -20,11 +23,33 @@ scores.each do |s|
   end
 end
 
-if frames.length == 11
+case frames.length
+when 11
   frames[9].concat(frames[10])
   frames.delete_at(10)
-elsif frames.length == 12
+when 12
   frames[9].concat(frames[10], frames[11])
   frames.delete_at(10)
   frames.delete_at(10)
 end
+frames[9].concat(shots) unless shots.empty?
+
+# ストライクとスペア時のボーナスポイントを追加して、フレームの合計を出す
+point = 0
+frames.each_with_index do |frame, idx|
+  if frame == [10] && frames[idx + 1] == [10] && frames[idx + 2] == [10] # 3回連続ストライク
+    point += 30
+  elsif frame == [10] && frames[idx + 1] == [10] # 2回連続ストライク
+    point += 20
+    point += frames[idx + 2][0]
+  elsif frame == [10] # 1回ストライク
+    point += 10
+    point += frames[idx + 1][0] + frames[idx + 1][1]
+  elsif frame.sum == 10 && idx != 9 # スペア（10フレーム目以外）
+    point += 10
+    point += frames[idx + 1][0]
+  else
+    point += frame.sum
+  end
+end
+puts point
