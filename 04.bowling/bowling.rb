586f37711794f4519c130e05a3cc69fda33dfdb0
emiko <31835314+siroemk@users.noreply.github.com>
2021-02-19 16:13:03 +0900

    三項演算子でコード短縮、10フレーム目作成箇所の修正、each_with_index.sumに変更

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -7,12 +7,7 @@ scores = score.split(',')
 shots = []
 frames = []
 scores.each do |s|
-  shots <<
-    if s == 'X'
-      10
-    else
-      s.to_i
-    end
+  shots << (s == 'X' ? 10 : s.to_i)
 
   if shots.count == 1 && shots.first == 10
     frames << [10]
@@ -25,31 +20,26 @@ end
 
 case frames.length
 when 11
-  frames[9].concat(frames[10])
-  frames.delete_at(10)
+  frames[9].concat(frames[10].pop(2))
 when 12
-  frames[9].concat(frames[10], frames[11])
-  frames.delete_at(10)
-  frames.delete_at(10)
+  frames[9].concat(frames[10].pop(1), frames[11].pop(1))
 end
 frames[9].concat(shots) unless shots.empty?
+frames.delete_if(&:empty?)
 
 # ストライクとスペア時のボーナスポイントを追加して、フレームの合計を出す
-point = 0
-frames.each_with_index do |frame, idx|
-  if frame == [10] && frames[idx + 1] == [10] && frames[idx + 2] == [10] # 3回連続ストライク
-    point += 30
-  elsif frame == [10] && frames[idx + 1] == [10] # 2回連続ストライク
-    point += 20
-    point += frames[idx + 2][0]
-  elsif frame == [10] # 1回ストライク
-    point += 10
-    point += frames[idx + 1][0] + frames[idx + 1][1]
-  elsif frame.sum == 10 && idx != 9 # スペア（10フレーム目以外）
-    point += 10
-    point += frames[idx + 1][0]
-  else
-    point += frame.sum
+point =
+  frames.each_with_index.sum do |frame, idx|
+    if frame == [10] && frames[idx + 1] == [10] && frames[idx + 2] == [10] # 3回連続ストライク
+      30
+    elsif frame == [10] && frames[idx + 1] == [10] # 2回連続ストライク
+      20 + frames[idx + 2][0]
+    elsif frame == [10] # 1回ストライク
+      10 + frames[idx + 1][0] + frames[idx + 1][1]
+    elsif frame.sum == 10 && idx != 9 # スペア（10フレーム目以外）
+      10 + frames[idx + 1][0]
+    else
+      frame.sum
+    end
   end
-end
 puts point
