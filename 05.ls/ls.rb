142848d19995ab20c699c04b19136217bed127c5
emiko <31835314+siroemk@users.noreply.github.com>
2021-03-10 15:32:39 +0900

    メソッド名の修正、ブロックの構造等の修正

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -5,16 +5,15 @@ require 'optparse'
 require 'etc'
 
 def main(options)
-  files = options['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*').sort
+  files = options['a'] ? Dir.glob('*', File::FNM_DOTMATCH).sort : Dir.glob('*').sort
   files = files.reverse if options['r']
 
   if options['l']
-    blocks = 0
-    files.each { |file| blocks += blocks(file) }
+    blocks = files.sum { |file| blocks(file) }
     puts "total #{blocks}"
     files.each { |file| puts l_option_data(file).join('  ') }
   else
-    no_option(files)
+    no_option_data(files).each { |display| puts display.join('    ') }
   end
 end
 
@@ -29,7 +28,7 @@ end
 def l_option_data(file)
   mode_number = format('%o', stat(file).mode)
   data = []
-  data << convert_to_filetype(stat(file).ftype) + convert_to_mode(mode_number[-3]) + convert_to_mode(mode_number[-2]) + convert_to_mode(mode_number[-1])
+  data << to_filetype_character(stat(file).ftype) + to_mode_character(mode_number[-3]) + to_mode_character(mode_number[-2]) + to_mode_character(mode_number[-1])
   data << stat(file).nlink
   data << Etc.getpwuid(stat(file).uid).name
   data << Etc.getgrgid(stat(file).gid).name
@@ -38,7 +37,7 @@ def l_option_data(file)
   data << file
 end
 
-def convert_to_filetype(ftype)
+def to_filetype_character(ftype)
   {
     file: '-',
     directory: 'd',
@@ -46,7 +45,7 @@ def convert_to_filetype(ftype)
   }[ftype.to_sym]
 end
 
-def convert_to_mode(mode)
+def to_mode_character(mode)
   {
     '0': '---',
     '1': '--x',
@@ -59,25 +58,22 @@ def convert_to_mode(mode)
   }[mode.to_sym]
 end
 
-def no_option(files)
+def no_option_data(files)
   # 3列表示のためにeach_sliceで、3つの要素をもつ配列を作る。
   slice_number = (files.size % 3).zero? ? files.size / 3 : files.size / 3 + 1
-  sliced_array = []
-  files.each_slice(slice_number) { |element| sliced_array << element }
+  sliced_array = files.each_slice(slice_number).to_a
 
   # transposeメソッドを使いたいので、配列内の要素の数を揃える。要素が足りない配列に空文字追加する。
   third_array = sliced_array.last
   (slice_number - third_array.size).times { third_array.push('') } if third_array.size < slice_number
 
   # 3列表示で文字を揃えたいので配列の中で一番長い文字列を基準に、空白を追加する。
-  array_with_blank = []
-  sliced_array.each do |str_array|
-    max_string = str_array.max_by(&:size).size
-    array_with_blank << str_array.each.map { |str| str + (' ' * (max_string - str.size)) }
-  end
-
-  transposed_array = array_with_blank.transpose
-  transposed_array.each { |display| puts display.join('    ') }
+  array_with_blank =
+    sliced_array.map do |str_array|
+      max_string = str_array.max_by(&:size).size
+      str_array.map { |str| str + (' ' * (max_string - str.size)) }
+    end
+  array_with_blank.transpose
 end
 
 options = ARGV.getopts('a', 'l', 'r')
