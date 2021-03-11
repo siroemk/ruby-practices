982d2b6b391108ef0bfc772e7dc76b9e33d17c59
emiko <31835314+siroemk@users.noreply.github.com>
2021-03-11 13:09:14 +0900

    wcファイルの追加

---
+++ 06.wc/wc.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'optparse'
+
+options = ARGV.getopts('l')
+file_array = ARGV
+
+total_lines = 0
+total_words = 0
+total_bytesize = 0
+
+if options['l']
+  file_array.each do |file|
+    sentence = File.read(file)
+    print sentence.lines.count.to_s.rjust(8)
+    puts " #{file}"
+      
+    total_lines += sentence.lines.count
+    total_words += sentence.split(/\s+/).count
+    total_bytesize += sentence.bytesize
+  end
+
+  unless file_array[1] == nil
+    print total_lines.to_s.rjust(8)
+    puts " total"
+  end
+end
+
+unless options['l']
+  file_array.each do |file|
+    sentence = File.read(file)
+    print sentence.lines.count.to_s.rjust(8)
+    print sentence.split(/\s+/).count.to_s.rjust(8)
+    print sentence.bytesize.to_s.rjust(8) 
+    puts " #{file}"
+    
+    total_lines += sentence.lines.count
+    total_words += sentence.split(/\s+/).count
+    total_bytesize += sentence.bytesize
+  end
+
+  unless file_array[1] == nil
+    print total_lines.to_s.rjust(8)
+    print total_words.to_s.rjust(8)
+    print total_bytesize.to_s.rjust(8)
+    puts " total"
+  end
+end
