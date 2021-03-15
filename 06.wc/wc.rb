b6fe8a64d2c17cca48ede5a7fb8eb7bbb002590c
emiko <31835314+siroemk@users.noreply.github.com>
2021-03-15 16:37:49 +0900

    メソッド化した

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,54 +3,75 @@
 
 require 'optparse'
 
+def main(options)
+  if ARGV[0] == nil # 標準出力
+    input_text = $stdin.readlines
+    display_array = []
+    display_array << readlines(input_text)
+  else # コマンドライン引数にファイルを指定
+    display_array = input_file
+  end
+
+  display_array.each do |array|
+    if options['l']
+      ARGV[0] == nil ? (print array.first) : (print array.first + array.last)
+    else
+      array.each { |data| print data }
+    end
+    puts ""
+  end
+end
+
+def readlines(input_text)
+  array = [to_lines(input_text), to_words(input_text.join), to_bytesize(input_text.join)]
+  array.map { |element| add_spaces(element) }
+end
+
+def input_file
+  file_array = ARGV
+  array_with_spaces =
+    file_array.map do |file|
+      text = File.read(file)
+      array = [to_lines(text.lines), to_words(text), to_bytesize(text)]
+      array.map { |element| add_spaces(element) }.push(" #{file}")
+    end
+
+  unless file_array[1] == nil # コマンドライン引数に複数ファイルを渡した場合
+    to_total(file_array, array_with_spaces)
+  end
+  array_with_spaces
+end
+
+def to_total(file_array, array_with_spaces)
+  total_lines = 0
+  total_words = 0
+  total_bytesize = 0
+  file_array.each do |file|
+    text = File.read(file)
+    total_lines += to_lines(text.lines)
+    total_words += to_words(text)
+    total_bytesize += to_bytesize(text)
+  end
+  array = [total_lines, total_words, total_bytesize]
+  total_array_with_spaces = array.map { |element| add_spaces(element) }.push(" total")
+  array_with_spaces.push(total_array_with_spaces)
+end
+
+def to_lines(text)
+  text.count
+end
+
+def to_words(text)
+  text.split(/\s+/).count
+end
+
+def to_bytesize(text)
+  text.bytesize
+end
+
+def add_spaces(display_element)
+  display_element.to_s.rjust(8)
+end
+
 options = ARGV.getopts('l')
-file_array = ARGV
-
-if ARGV[0] == nil
-  input = $stdin.readlines
-  print input.size.to_s.rjust(8)
-  print input.join.split(/\s+/).count.to_s.rjust(8)
-  puts input.join.bytesize.to_s.rjust(8)
-end
-
-total_lines = 0
-total_words = 0
-total_bytesize = 0
-
-if options['l']
-  file_array.each do |file|
-    sentence = File.read(file)
-    print sentence.lines.count.to_s.rjust(8)
-    puts " #{file}"
-      
-    total_lines += sentence.lines.count
-    total_words += sentence.split(/\s+/).count
-    total_bytesize += sentence.bytesize
-  end
-
-  unless file_array[1] == nil
-    print total_lines.to_s.rjust(8)
-    puts " total"
-  end
-end
-
-unless options['l']
-  file_array.each do |file|
-    sentence = File.read(file)
-    print sentence.lines.count.to_s.rjust(8)
-    print sentence.split(/\s+/).count.to_s.rjust(8)
-    print sentence.bytesize.to_s.rjust(8) 
-    puts " #{file}"
-    
-    total_lines += sentence.lines.count
-    total_words += sentence.split(/\s+/).count
-    total_bytesize += sentence.bytesize
-  end
-
-  unless file_array[1] == nil
-    print total_lines.to_s.rjust(8)
-    print total_words.to_s.rjust(8)
-    print total_bytesize.to_s.rjust(8)
-    puts " total"
-  end
-end
+main(options)
