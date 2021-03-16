d8332ead2cce55b42cdd4e6e6b644bdfdebbfe7e
emiko <31835314+siroemk@users.noreply.github.com>
2021-03-16 14:56:21 +0900

    add_spacesメソッドを修正して、rubocopを通した

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,7 +4,7 @@
 require 'optparse'
 
 def main(options)
-  if ARGV[0] == nil # 標準出力
+  if ARGV[0].nil? # 標準出力
     input_text = $stdin.readlines
     display_array = []
     display_array << readlines(input_text)
@@ -14,17 +14,17 @@ def main(options)
 
   display_array.each do |array|
     if options['l']
-      ARGV[0] == nil ? (print array.first) : (print array.first + array.last)
+      ARGV[0].nil? ? (print array.first) : (print array.first + array.last)
     else
       array.each { |data| print data }
     end
-    puts ""
+    puts ''
   end
 end
 
 def readlines(input_text)
   array = [to_lines(input_text), to_words(input_text.join), to_bytesize(input_text.join)]
-  array.map { |element| add_spaces(element) }
+  add_spaces(array)
 end
 
 def input_file
@@ -33,12 +33,10 @@ def input_file
     file_array.map do |file|
       text = File.read(file)
       array = [to_lines(text.lines), to_words(text), to_bytesize(text)]
-      array.map { |element| add_spaces(element) }.push(" #{file}")
+      add_spaces(array).push(" #{file}")
     end
 
-  unless file_array[1] == nil # コマンドライン引数に複数ファイルを渡した場合
-    to_total(file_array, array_with_spaces)
-  end
+  to_total(file_array, array_with_spaces) unless file_array[1].nil? # コマンドライン引数に複数ファイルを渡した場合
   array_with_spaces
 end
 
@@ -53,7 +51,7 @@ def to_total(file_array, array_with_spaces)
     total_bytesize += to_bytesize(text)
   end
   array = [total_lines, total_words, total_bytesize]
-  total_array_with_spaces = array.map { |element| add_spaces(element) }.push(" total")
+  total_array_with_spaces = add_spaces(array).push(' total')
   array_with_spaces.push(total_array_with_spaces)
 end
 
@@ -69,8 +67,8 @@ def to_bytesize(text)
   text.bytesize
 end
 
-def add_spaces(display_element)
-  display_element.to_s.rjust(8)
+def add_spaces(array)
+  array.map { |element| element.to_s.rjust(8) }
 end
 
 options = ARGV.getopts('l')
